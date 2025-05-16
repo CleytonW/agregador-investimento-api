8ba1a0a57d853b75b4086d03d61ea20a9631b3f2
Cleyton Melo <cleytonw11@gmail.com>
2025-05-16 15:46:45 -0300

    Criando repository para API gravar informações no banco de dados

---
+++ src/main/java/br/com/agin/api/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package br.com.agin.api.repository;
+
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.agin.api.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    
+
+    
+}
