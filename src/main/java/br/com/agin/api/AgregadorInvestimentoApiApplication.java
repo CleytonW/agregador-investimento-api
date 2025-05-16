7aab16d8a735718cab645773c80dcb4f021b9156
Cleyton Melo <cleytonw11@gmail.com>
2025-05-16 14:34:20 -0300

    Iniciando API para CRUD de usuários

---
+++ src/main/java/br/com/agin/api/AgregadorInvestimentoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.agin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorInvestimentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorInvestimentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agin/api/AgregadorInvestimentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorInvestimentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
